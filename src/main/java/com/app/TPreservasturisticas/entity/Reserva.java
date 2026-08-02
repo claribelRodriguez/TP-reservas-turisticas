0729aaa4a3f528ffae5f845789439b2e70d52b90
Claribel Rodriguez <rodriguezgclaribel@gmail.com>
2026-08-02 12:49:13 -0500

    Se agrega capa entity y repository básicos

---
+++ src/main/java/com/app/TPreservasturisticas/entity/Reserva.java
@@ -0,0 +1,4 @@
+package com.app.TPreservasturisticas.entity;
+
+public class Reserva {
+}
---
+++ src/main/java/com/app/TPreservasturisticas/repository/ActividadRepository.java
@@ -0,0 +1,4 @@
+package com.app.TPreservasturisticas.repository;
+
+public interface ActividadRepository {
+}
