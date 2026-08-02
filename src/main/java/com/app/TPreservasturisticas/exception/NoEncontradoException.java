3c7009631183ccd85caf6b014eed1d09b3e21774
Claribel Rodriguez <rodriguezgclaribel@gmail.com>
2026-08-02 14:27:34 -0500

    Implementación de Actividad service y Actividad controller

---
+++ src/main/java/com/app/TPreservasturisticas/exception/NoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.app.TPreservasturisticas.exception;
+
+public class NoEncontradoException extends RuntimeException {
+  public NoEncontradoException(String message) {
+    super(message);
+  }
+}
