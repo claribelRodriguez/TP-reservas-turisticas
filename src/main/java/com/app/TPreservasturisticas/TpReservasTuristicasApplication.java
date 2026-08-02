190075ef375933fcc0e439f04e5de9cb247308c5
Claribel Rodriguez <rodriguezgclaribel@gmail.com>
2026-08-02 11:30:00 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/app/TPreservasturisticas/TpReservasTuristicasApplication.java
@@ -0,0 +1,13 @@
+package com.app.TPreservasturisticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpReservasTuristicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpReservasTuristicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/TPreservasturisticas/TpReservasTuristicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.TPreservasturisticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpReservasTuristicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
